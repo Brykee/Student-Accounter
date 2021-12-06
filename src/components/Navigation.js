e29a5296ad01b979105d0ad4f3e55438b6824241
Bryce Richardson <drichardson2@harding.edu>
2021-12-06 13:03:12 -0600

    name change

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -23,7 +23,7 @@ export default function Navigation() {
     <>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="/">Bet Brawlers</Navbar.Brand>
+          <Navbar.Brand href="/">Student Accounter</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className=" justify-content-end">
