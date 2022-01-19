a09f941498414b40e6cafddd1b3d242bbb0bb77d
Bryce Richardson <74836531+Brykee@users.noreply.github.com>
2022-01-19 17:33:14 -0600

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import './App.css';
-import { db } from '../firebase';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.scss";
+import { db } from "../firebase";
 import {
   collection,
   getDocs,
@@ -11,52 +11,53 @@ import {
   doc,
   query,
   orderBy,
-} from 'firebase/firestore';
-import Navigation from './Navigation';
-import { useAuth } from '../contexts/AuthContext';
-
+} from "firebase/firestore";
+import Navigation from "./Navigation";
+import { useAuth } from "../contexts/AuthContext";
 export default function Dashboard() {
   const auth = useAuth();
-
-  const [newName, setNewName] = useState('');
+  const [newName, setNewName] = useState("");
   const [newTardy, setNewTardy] = useState(0);
-
+  const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(
     db,
-    'users/' + auth.currentUser.uid + '/data/'
+    "users/" + auth.currentUser.uid + "/data/"
   );
-
   const createUser = async () => {
+    setLoading(true);
     await addDoc(usersCollectionRef, {
       name: newName,
       tardy: Number(newTardy),
     }).then(() => {
-      alert('User Created');
+      setLoading(false);
     });
   };
-
   const updateUser = async (id, tardy) => {
-    const userDoc = doc(db, 'users/' + auth.currentUser.uid + '/data/', id);
+    setLoading(true);
+    const userDoc = doc(db, "users/" + auth.currentUser.uid + "/data/", id);
     const newFields = { tardy: tardy + 1 };
     await updateDoc(userDoc, newFields);
+    setLoading(false);
   };
-
   const deleteUser = async (id) => {
-    const userDoc = doc(db, 'users/' + auth.currentUser.uid + '/data/', id);
+    setLoading(true);
+    const userDoc = doc(db, "users/" + auth.currentUser.uid + "/data/", id);
     await deleteDoc(userDoc);
+    setLoading(false);
   };
-
   useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(
-        query(usersCollectionRef, orderBy('tardy', 'desc'))
-      );
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    getUsers();
-  }, []);
-
+    if (!loading) {
+      const getUsers = async () => {
+        const data = await getDocs(
+          query(usersCollectionRef, orderBy("tardy", "desc"))
+        );
+        setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      };
+      getUsers();
+    }
+  }, [loading]);
+  // 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
   return (
     <>
       <Navigation />
@@ -77,30 +78,387 @@ export default function Dashboard() {
         />
         <br />
         <button onClick={createUser}> Create User</button>
-
-        {users.map((user) => {
-          return (
-            <div>
-              <br />
-              <h2>Name: {user.name}</h2>
-              <h2>Tardies: {user.tardy}</h2>
-              <button
-                onClick={() => {
-                  updateUser(user.id, user.tardy);
-                }}
-              >
-                Add Tardy
-              </button>
-              <button
-                onClick={() => {
-                  deleteUser(user.id);
-                }}
-              >
-                Delete User
-              </button>
+        {loading ? (
+          <div className="row">
+            <div className="col-12 text-center">
+              <div class="spinner-border" role="status"></div>
             </div>
-          );
-        })}
+          </div>
+        ) : (
+          <>
+            {users.map((user) => {
+              return (
+                <div>
+                  <br />
+                  <h2>Name: {user.name}</h2>
+                  <h2>Tardies: {user.tardy}</h2>
+                  <button
+                    onClick={() => {
+                      updateUser(user.id, user.tardy);
+                    }}
+                  >
+                    Add Tardy
+                  </button>
+                  <button
+                    onClick={() => {
+                      deleteUser(user.id);
+                    }}
+                  >
+                    Delete User
+                  </button>
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
+    </>
+  );
+}import React from "react";
+import { useState, useEffect } from "react";
+import "./App.scss";
+import { db } from "../firebase";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import Navigation from "./Navigation";
+import { useAuth } from "../contexts/AuthContext";
+export default function Dashboard() {
+  const auth = useAuth();
+  const [newName, setNewName] = useState("");
+  const [newTardy, setNewTardy] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(
+    db,
+    "users/" + auth.currentUser.uid + "/data/"
+  );
+  const createUser = async () => {
+    setLoading(true);
+    await addDoc(usersCollectionRef, {
+      name: newName,
+      tardy: Number(newTardy),
+    }).then(() => {
+      setLoading(false);
+    });
+  };
+  const updateUser = async (id, tardy) => {
+    setLoading(true);
+    const userDoc = doc(db, "users/" + auth.currentUser.uid + "/data/", id);
+    const newFields = { tardy: tardy + 1 };
+    await updateDoc(userDoc, newFields);
+    setLoading(false);
+  };
+  const deleteUser = async (id) => {
+    setLoading(true);
+    const userDoc = doc(db, "users/" + auth.currentUser.uid + "/data/", id);
+    await deleteDoc(userDoc);
+    setLoading(false);
+  };
+  useEffect(() => {
+    if (!loading) {
+      const getUsers = async () => {
+        const data = await getDocs(
+          query(usersCollectionRef, orderBy("tardy", "desc"))
+        );
+        setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      };
+      getUsers();
+    }
+  }, [loading]);
+  // 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
+  return (
+    <>
+      <Navigation />
+      <div className="App">
+        <h1>Tardy List</h1>
+        <input
+          placeholder="Name..."
+          onChange={(event) => {
+            setNewName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Number of Tardies..."
+          onChange={(event) => {
+            setNewTardy(event.target.value);
+          }}
+        />
+        <br />
+        <button onClick={createUser}> Create User</button>
+        {loading ? (
+          <div className="row">
+            <div className="col-12 text-center">
+              <div class="spinner-border" role="status"></div>
+            </div>
+          </div>
+        ) : (
+          <>
+            {users.map((user) => {
+              return (
+                <div>
+                  <br />
+                  <h2>Name: {user.name}</h2>
+                  <h2>Tardies: {user.tardy}</h2>
+                  <button
+                    onClick={() => {
+                      updateUser(user.id, user.tardy);
+                    }}
+                  >
+                    Add Tardy
+                  </button>
+                  <button
+                    onClick={() => {
+                      deleteUser(user.id);
+                    }}
+                  >
+                    Delete User
+                  </button>
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
+    </>
+  );
+}import React from "react";
+import { useState, useEffect } from "react";
+import "./App.scss";
+import { db } from "../firebase";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import Navigation from "./Navigation";
+import { useAuth } from "../contexts/AuthContext";
+export default function Dashboard() {
+  const auth = useAuth();
+  const [newName, setNewName] = useState("");
+  const [newTardy, setNewTardy] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(
+    db,
+    "users/" + auth.currentUser.uid + "/data/"
+  );
+  const createUser = async () => {
+    setLoading(true);
+    await addDoc(usersCollectionRef, {
+      name: newName,
+      tardy: Number(newTardy),
+    }).then(() => {
+      setLoading(false);
+    });
+  };
+  const updateUser = async (id, tardy) => {
+    setLoading(true);
+    const userDoc = doc(db, "users/" + auth.currentUser.uid + "/data/", id);
+    const newFields = { tardy: tardy + 1 };
+    await updateDoc(userDoc, newFields);
+    setLoading(false);
+  };
+  const deleteUser = async (id) => {
+    setLoading(true);
+    const userDoc = doc(db, "users/" + auth.currentUser.uid + "/data/", id);
+    await deleteDoc(userDoc);
+    setLoading(false);
+  };
+  useEffect(() => {
+    if (!loading) {
+      const getUsers = async () => {
+        const data = await getDocs(
+          query(usersCollectionRef, orderBy("tardy", "desc"))
+        );
+        setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      };
+      getUsers();
+    }
+  }, [loading]);
+  // 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
+  return (
+    <>
+      <Navigation />
+      <div className="App">
+        <h1>Tardy List</h1>
+        <input
+          placeholder="Name..."
+          onChange={(event) => {
+            setNewName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Number of Tardies..."
+          onChange={(event) => {
+            setNewTardy(event.target.value);
+          }}
+        />
+        <br />
+        <button onClick={createUser}> Create User</button>
+        {loading ? (
+          <div className="row">
+            <div className="col-12 text-center">
+              <div class="spinner-border" role="status"></div>
+            </div>
+          </div>
+        ) : (
+          <>
+            {users.map((user) => {
+              return (
+                <div>
+                  <br />
+                  <h2>Name: {user.name}</h2>
+                  <h2>Tardies: {user.tardy}</h2>
+                  <button
+                    onClick={() => {
+                      updateUser(user.id, user.tardy);
+                    }}
+                  >
+                    Add Tardy
+                  </button>
+                  <button
+                    onClick={() => {
+                      deleteUser(user.id);
+                    }}
+                  >
+                    Delete User
+                  </button>
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
+    </>
+  );
+}import React from "react";
+import { useState, useEffect } from "react";
+import "./App.scss";
+import { db } from "../firebase";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import Navigation from "./Navigation";
+import { useAuth } from "../contexts/AuthContext";
+export default function Dashboard() {
+  const auth = useAuth();
+  const [newName, setNewName] = useState("");
+  const [newTardy, setNewTardy] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(
+    db,
+    "users/" + auth.currentUser.uid + "/data/"
+  );
+  const createUser = async () => {
+    setLoading(true);
+    await addDoc(usersCollectionRef, {
+      name: newName,
+      tardy: Number(newTardy),
+    }).then(() => {
+      setLoading(false);
+    });
+  };
+  const updateUser = async (id, tardy) => {
+    setLoading(true);
+    const userDoc = doc(db, "users/" + auth.currentUser.uid + "/data/", id);
+    const newFields = { tardy: tardy + 1 };
+    await updateDoc(userDoc, newFields);
+    setLoading(false);
+  };
+  const deleteUser = async (id) => {
+    setLoading(true);
+    const userDoc = doc(db, "users/" + auth.currentUser.uid + "/data/", id);
+    await deleteDoc(userDoc);
+    setLoading(false);
+  };
+  useEffect(() => {
+    if (!loading) {
+      const getUsers = async () => {
+        const data = await getDocs(
+          query(usersCollectionRef, orderBy("tardy", "desc"))
+        );
+        setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      };
+      getUsers();
+    }
+  }, [loading]);
+  // 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
+  return (
+    <>
+      <Navigation />
+      <div className="App">
+        <h1>Tardy List</h1>
+        <input
+          placeholder="Name..."
+          onChange={(event) => {
+            setNewName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Number of Tardies..."
+          onChange={(event) => {
+            setNewTardy(event.target.value);
+          }}
+        />
+        <br />
+        <button onClick={createUser}> Create User</button>
+        {loading ? (
+          <div className="row">
+            <div className="col-12 text-center">
+              <div class="spinner-border" role="status"></div>
+            </div>
+          </div>
+        ) : (
+          <>
+            {users.map((user) => {
+              return (
+                <div>
+                  <br />
+                  <h2>Name: {user.name}</h2>
+                  <h2>Tardies: {user.tardy}</h2>
+                  <button
+                    onClick={() => {
+                      updateUser(user.id, user.tardy);
+                    }}
+                  >
+                    Add Tardy
+                  </button>
+                  <button
+                    onClick={() => {
+                      deleteUser(user.id);
+                    }}
+                  >
+                    Delete User
+                  </button>
+                </div>
+              );
+            })}
+          </>
+        )}
       </div>
     </>
   );
